1b8d501a03ebb842832f2774923ee40970028fea
rshved <shvietsovroman@gmail.com>
2021-04-02 15:27:29 +0300

    firt commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consequatur a sed molestias tempora ea nulla incidunt ut in unde praesentium omnis ex veniam placeat ratione voluptate, architecto rerum, dolor assumenda tempore inventore! Quae dolorem neque quisquam sunt aliquid quaerat a pariatur corporis cupiditate iste asperiores laboriosam, quis saepe hic ad nostrum eius voluptatum laborum? Ratione illo ea voluptatum dolore similique! Officiis sequi quod harum error ducimus distinctio repellendus quam. Minima reprehenderit nobis est officiis ex voluptatibus, vel iusto. Quis molestias facilis cumque praesentium nesciunt, beatae sed. Blanditiis dolores molestiae ex, vel aliquam officia. Maxime ipsa facilis magni deleniti error.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corrupti voluptates, impedit facilis laudantium at blanditiis inventore dolor! Dicta cumque harum placeat vitae enim ipsam similique minus adipisci eaque, aspernatur magni facilis nihil explicabo, blanditiis labore vel obcaecati quis. Doloremque, delectus ab molestias facere a aut hic ea iure expedita?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, asperiores provident, id quas aspernatur quam ea aliquam itaque, perspiciatis mollitia fuga voluptatum laborum ducimus? Corrupti?</p>
+
   </div>
 </template>
